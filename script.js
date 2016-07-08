f277fc19a856c8c1dc3b1faabd7c0edd97a8a66c
Elizabeth Kipp <liz@elizabethkipp.com>
2016-07-08 16:13:36 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+var $catPic = $('.catPic');
+	
+$catPic.click(function(e) {
+  //the element has been clicked... do stuff here
+  
+});
+
+	console.log("this works");
+})
