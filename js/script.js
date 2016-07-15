91d8912b0e76e4ce16fd6c71dcba604a88e2cdbf
Elizabeth Kipp <liz@elizabethkipp.com>
2016-07-15 14:22:04 -0500

    organized bootstrap modals and images

--- js/script.js
+++ js/script.js
@@ -2,33 +2,38 @@
 $(document).ready(function() {
 
 //Array containing a cat name, image and count
-	var cats = [ 
-		{
-			'name': "Sleepy",
-			'image': "images/sleepyCat.png",
-			'count': 0;
-		},
-		{
-			'name': "Happy",
-			'image': "images/happyCat.png",
-			'count': 0;
-		},
-		{
-			'name': "Grumpy",
-			'image': "images/grumpyCat.png",
-			'count': 0;
-		},
-		{
-			'name': "Dopey",
-			'image': "images/dopeyCat.png",
-			'count': 0;
-		},
-		{
-			'name': "Doc",
-			'image': "images/sleepyCat.png",
-			'count': 0;
-		},
-	]
+	// var cats = [ 
+	// 	{
+	// 		'name': "Sleepy",
+	// 		'image': "images/sleepyCat.png",
+	// 		'count': 0;
+	// 	},
+	// 	{
+	// 		'name': "Happy",
+	// 		'image': "images/happyCat.png",
+	// 		'count': 0;
+	// 	},
+	// 	{
+	// 		'name': "Grumpy",
+	// 		'image': "images/grumpyCat.png",
+	// 		'count': 0;
+	// 	},
+	// 	{
+	// 		'name': "Dopey",
+	// 		'image': "images/dopeyCat.png",
+	// 		'count': 0;
+	// 	},
+	// 	{
+	// 		'name': "Doc",
+	// 		'image': "images/docCat.png",
+	// 		'count': 0;
+	// 	},
+	// 	{
+	// 		'name': "Bashful",
+	// 		'image': "images/bashfulCat.png",
+	// 		'count': 0;
+	// 	},
+	// ]
 
 
 });
