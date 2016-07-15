e001cca7aa1c182b86ca1e42b2855e3aaeb413b5
Elizabeth Kipp <liz@elizabethkipp.com>
2016-07-15 13:11:42 -0500

    added cat array, added 5 bootstrap columns in html

--- js/script.js
+++ js/script.js
@@ -1,28 +1,38 @@
 
 $(document).ready(function() {
 
-	var catName1 = 'Meany';
-	var catName2 = 'Nicey';
-	var counter1 = 0;
-	var counter2 = 0;
+//Array containing a cat name, image and count
+	var cats = [ 
+		{
+			'name': "Sleepy",
+			'image': "images/sleepyCat.png",
+			'count': 0;
+		},
+		{
+			'name': "Happy",
+			'image': "images/happyCat.png",
+			'count': 0;
+		},
+		{
+			'name': "Grumpy",
+			'image': "images/grumpyCat.png",
+			'count': 0;
+		},
+		{
+			'name': "Dopey",
+			'image': "images/dopeyCat.png",
+			'count': 0;
+		},
+		{
+			'name': "Doc",
+			'image': "images/sleepyCat.png",
+			'count': 0;
+		},
+	]
 
-	$('#cat-name1').html(catName1);
-	$('#cat-name2').html(catName2);
 
-
-	$('#meanPic').click(function() {
-		counter1++;
-		$('#meanCounter').html('You clicked ' + catName1 + ' ' + counter1 +  ' times');
-		console.log("# of clicks");
-	})
-
-	$('#cutePic').click(function() {
-		counter2++;
-		$('#cuteCounter').html('You clicked ' + catName2 + ' ' + counter2 +  ' times');
-		console.log("# of clicks");
-	})
 });
-
+console.log('cats');
 
 
 
